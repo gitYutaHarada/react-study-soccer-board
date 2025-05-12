a68dbf89543b4880fb82ac78c7e38f3e16f517c8
haradayuuta <mayutokuyuta@gmail.com>
2025-05-12 21:38:00 +0900

    ✨ Player欄の作成

--- src/components/Players/index.jsx
+++ src/components/Players/index.jsx
@@ -1,7 +1,14 @@
 import PlayerAll from "@/components/Players/PlayerAll";
 import classes from "@/components/Players/player.module.css";
 
-const Players = ({ squad }) => {
+const Players = async () => {
+  const token = process.env.FOOTBALL_DATA_API_TOKEN;
+  const res = await fetch("https://api.football-data.org/v4/teams/86", {
+    headers: { "X-Auth-Token": token },
+  });
+  const data = await res.json();
+  const squad = data.squad || [];
+
   return (
     <div className={classes.container}>
       <PlayerAll squad={squad} />
